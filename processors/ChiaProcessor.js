c3507f3957339b540ef5e1ec1a3f6559f32f77f3
Max Bachhuber <bahuma20@bahuma.io>
2017-03-13 20:09:31 +0100

    Refactoring

---
+++ processors/ChiaProcessor.js
@@ -0,0 +1,34 @@
+const Processor = require('./Processor');
+
+class ChiaProcessor extends Processor {
+
+  constructor(bot) {
+    super(bot);
+    let self = this;
+
+    self.on('text', msg => self.onText)
+  }
+
+  onText(msg) {
+    if (this.shouldReply(msg) && this.checkChia(msg.text)) {
+      this.bot.sendMessage(msg.chat.id, 'SUPERFOOD');
+    }
+  }
+
+  checkChia(text) {
+    text = text.toLowerCase();
+    return text.startsWith('chia') || text.indexOf(' chia') !== -1 || text.indexOf('-chia') !== -1;
+  }
+
+  shouldReply(msg) {
+    // DO NOT REPLY TO OLD MESSAGES
+    let currentTime = Date.now() / 1000;
+
+    if(msg.date < currentTime - 60) {
+      // If received message older than a minute, don't reply
+      return false;
+    }
+
+    return true;
+  }
+}
