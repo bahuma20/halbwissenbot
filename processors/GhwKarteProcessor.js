7a8e38af790aa9b9005188782b2fc1bf7dcff00f
Max Bachhuber <max.bachhuber@bahuma.io>
2017-03-16 14:39:30 +0100

    Use timeouts and fix bugs

--- processors/GhwKarteProcessor.js
+++ processors/GhwKarteProcessor.js
@@ -9,9 +9,11 @@ class GhwKarteProcessor extends Processor {
     self.ghwKarte = new GhwKarte();
     self.karteURL = 'https://halbwissenbot.herokuapp.com';
 
-    self.bot.onText(/\/ghwkarte/, function(msg) {
+    self.bot.onText(/\/ghwkarte$/, function(msg) {
       self.bot.sendMessage(msg.chat.id, `Die #ghwkarte findest du auf ${self.karteURL}`);
-      self.bot.sendMessage(msg.chat.id, 'Falls du deinen Standort auch zur #ghwkarte hinzufügen möchtest verwende "/ghwkarte ORT"');
+      setTimeout(() => {
+        self.bot.sendMessage(msg.chat.id, 'Falls du deinen Standort auch zur #ghwkarte hinzufügen möchtest verwende "/ghwkarte ORT"');
+      }, 1000);
     });
 
     self.bot.onText(/\/ghwkarte (.+)/, function (msg, match) {
@@ -20,7 +22,9 @@ class GhwKarteProcessor extends Processor {
       self.ghwKarte.addEntryWithGeocoding(location, location)
         .then(entry => {
           self.bot.sendMessage(msg.chat.id, 'Dein Standort wurde zur #ghwkarte hinzugefügt.');
-          self.bot.sendMessage(msg.chat.id, self.karteURL);
+          setTimeout(() => {
+            self.bot.sendMessage(msg.chat.id, self.karteURL);
+          }, 1000);
         }).catch(error => console.log(error));
     });
   }
