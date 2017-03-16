a81d0ce5237f0d7d25d7712e61180cb8dac15a79
Max Bachhuber <bahuma20@bahuma.io>
2017-03-16 11:25:32 +0100

    Added Web server and polymer and a map

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 worker: npm start
+
+web: npm serve
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use('/', express.static('public'));
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!');
+});
