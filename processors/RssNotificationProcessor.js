f1d161cb5e2952d0681eddf79d36e79aaa7cfc31
Max Bachhuber <bahuma20@bahuma.io>
2017-03-22 10:44:07 +0100

    Implementing stuff

--- processors/RssNotificationProcessor.js
+++ processors/RssNotificationProcessor.js
@@ -1,5 +1,7 @@
 const Processor = require('./Processor');
 const CronJob = require('cron').CronJob;
+const feedparser = require('feedparser-promised');
+const FeedStatus = require('../model/FeedStatus');
 
 class RssNotificationProcessor extends Processor {
 
@@ -14,13 +16,41 @@ class RssNotificationProcessor extends Processor {
   setupCronjob() {
     let self = this;
 
-    let job = new CronJob('* * * * * *', () => {
+    self.checkForNewReleases();
+
+    let job = new CronJob('0 * * * * *', () => {
       self.checkForNewReleases();
     }, null, true, 'Europe/Berlin');
   }
 
   checkForNewReleases() {
     console.log('check now');
+
+    const feedURL = 'http://halbwissen.co/feed/mp3';
+
+    feedparser.parse(feedURL)
+      .then(items => {
+        let guid = items[0].guid;
+
+        FeedStatus.findOne({}, 'lastGuid', (err, feedStatus) => {
+          console.log(feedStatus);
+        });
+      }).catch(error => {
+        console.log(error);
+      });
+  }
+
+  initialize() {
+    let feedStatus = new FeedStatus();
+    feedStatus.lastGuid = 0;
+    feedStatus.save();
+  }
+
+  updateFeedStatus(newGuid) {
+    FeedStatus.findOne({}, (err, feedStatus) => {
+      feedStatus.lastGuid = newGuid;
+      feedStatus.save();
+    });
   }
 }
 
